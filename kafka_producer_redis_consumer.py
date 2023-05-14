bcca84a444821e77f492a49d3735dfe1dbfe0847
alex_black <alexblacknn@yandex.ru>
2023-05-14 23:59:33 +0300

    final commit

--- kafka_producer_redis_consumer.py
+++ kafka_producer_redis_consumer.py
@@ -1,68 +1,74 @@
 """ Code for data creation and feeding into kafka """
+import random
+import string
+import uuid
+from datetime import datetime
+from time import sleep
+
 from kafka import KafkaProducer
 from redis import Redis
 
-from time import sleep
-import random
-import string
-from datetime import datetime
-import uuid
-
 NUM_SYMBOLS_IN_EMAIL = 7
 
-producer = KafkaProducer(
-    bootstrap_servers=['localhost:9092'],
-    value_serializer=lambda x: x
-)
+
+class CustomKafkaProducer:
+    def __init__(self, address):
+        self.producer = KafkaProducer(
+            bootstrap_servers=address,
+            value_serializer=lambda x: x
+        )
+
+    def send(self, topic, value, key):
+        self.producer.send(
+            topic,
+            value=value.encode(),
+            key=key.encode()
+        )
+
+    def close(self):
+        self.producer.close()
 
 
-def get_current_time():
-    now = datetime.now()
-    return now.strftime("%Y-%m-%d %H:%M:%S")
-
-
-def push():
-    id = 0
-    timestamp = 1683964363
+def create_kafka_test_data():
+    # Итерируем timestamp просмотра
+    timestamp = 0
     while True:
-        # Итерируем айди записи и timestamp просмотра
-        id += 1
         timestamp += 1
-
         # Создаем случайный ключ (в конце запятая обязательна!)
         email = ''.join(random.choices(
             string.ascii_uppercase, k=NUM_SYMBOLS_IN_EMAIL)
         ) + '@mail.com'
         film_id = str(uuid.uuid4())
         key = email + film_id + ','
+        current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        user_generated_content = f'{email}, {film_id}, "{current_time}", {timestamp}'
+        yield user_generated_content, key
 
-        # Создаем пользовательские данные
-        user_generated_content = f'{email}, {film_id}, "{get_current_time()}", {timestamp}'
 
+def push(kafka_producer, redis):
+    for user_generated_content, key in create_kafka_test_data():
+        # Отправляем данные в кафку
+        kafka_producer.send('user_film_timestamp', user_generated_content, key)
 
-        #Отправляем данные в кафку
-        producer.send(
-            'user_film_timestamp',
-            value=user_generated_content.encode(),
-            key=key.encode()
-        )
-
-        print('-' * 100)
         print(
-            f'produced: \n\t key:  {key} \n\t message: {user_generated_content}'
+            f'{"-" * 100} \n produced: \n\t key:  {key} \n\t message: {user_generated_content}'
         )
 
         sleep(3)
-        redis = Redis(host=f'127.0.0.1', port=6379, db=1)
+
         message = redis.get(key)
 
         print(
             f'redis: \n\t key:  {key} \n\t message: {message}'
         )
 
-try:
-    push()
-except KeyboardInterrupt:
-    producer.close()
-    print("exit")
 
+if __name__ == '__main__':
+    kafka_producer = CustomKafkaProducer(['localhost:9092'])
+    redis = Redis(host=f'127.0.0.1', port=6379, db=1)
+
+    try:
+        push(kafka_producer, redis)
+    except KeyboardInterrupt:
+        kafka_producer.close()
+        print("exit")
