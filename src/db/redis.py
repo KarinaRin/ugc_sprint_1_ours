192c71e8999e0ea94a591ae69c2de4bc47f18116
karinarin <beketaeva.karina@gmail.com>
2023-05-18 20:11:41 +0300

    fix annotate

--- src/db/redis.py
+++ src/db/redis.py
@@ -9,11 +9,11 @@ class CustomRedis:
     def __init__(self, host: str, port: int, db: int):
         self.redis = Redis(host=host, port=port, db=db)
 
-    async def _get_message(self, key: str):
+    async def _get_message(self, key: str) -> str:
         message = await self.redis.get(key)
         return message.decode()
 
-    async def get_timestamp(self, key: str):
+    async def get_timestamp(self, key: str) -> str:
         message = await self._get_message(key)
         return message.split(',')[3]
 
